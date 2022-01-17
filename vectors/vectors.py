1711eeaad2c7d6d16a44f8fbc85d8b367b2f544f
Cvaniak <igna.cwaniak@gmail.com>
2022-01-17 23:23:20 +0100

    Changed to not use tuples -> mutable once again

--- vectors/vectors.py
+++ vectors/vectors.py
@@ -3,12 +3,40 @@ from __future__ import division
 from functools import reduce
 import math
 from numbers import Real
-from typing import NamedTuple
 
-class Point(NamedTuple):
-    x: float
-    y: float
-    z: float
+def _set_in_list(values, index, new_value):
+    values[index] = new_value
+    
+def _link_with_list(index):
+    return property(lambda self: self._values[index],
+                    lambda self, val: _set_in_list(self._values, index, val))
+
+class Point:
+    x = _link_with_list(0) 
+    y = _link_with_list(1) 
+    z = _link_with_list(2) 
+
+    def __init__(self, x, y, z=0):
+        self._values = [x, y, z]
+
+    def __eq__(self, pt):
+        return all([a == b for a, b in zip(self, pt)])
+
+    def __iter__(self):
+        for xyz in self._values:
+            yield xyz
+
+    def __getitem__(self, index):
+        return self._values[index]
+
+    def __len__(self):
+        return len(self._values)
+
+    def __contains__(self, item):
+        return item in self._values
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}{tuple(self._values)}"
 
     def __sub__(self, pt):
         """Return a Point instance as the displacement of two points."""
@@ -22,11 +50,20 @@ class Point(NamedTuple):
         return self.__class__(*[a+b for a, b in zip(self, pt)])
 
     def to_dict(self):
-        return self._asdict()
+        return {"x":self.x, "y":self.y, "z":self.z}
     
     def to_list(self):
         '''Returns an array of [x,y,z] of the end points'''
-        return list(self) 
+        return self._values
+
+    def xy(self):
+        return (self.x, self.y)
+
+    def xz(self):
+        return (self.x, self.z)
+
+    def yz(self):
+        return (self.y, self.z)
 
     @classmethod
     def from_list(cls, l):
@@ -45,13 +82,13 @@ class Vector(Point):
     Cylindrical coordinates in the r, theta, z space.(Cylindrical class method)
     """
 
-    def __new__(cls, *args, **kwargs):
-        return super(Vector, cls).__new__(cls, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        return super(Vector, self).__init__(*args, **kwargs)
 
     def __add__(self, obj):
         """Add two vectors together"""
         if type(obj) is type(self):
-            return self + obj
+            return self.__class__(*[a+b for a, b in zip(self, obj)])
         elif isinstance(obj, Real):
             return self.add(obj)
         raise TypeError
@@ -59,7 +96,7 @@ class Vector(Point):
     def __sub__(self, obj):
         """Subtract two vectors"""
         if type(obj) is type(self):
-            return self - obj
+            return self.__class__(*[a-b for a, b in zip(self, obj)])
         elif isinstance(obj, Real):
             return self.subtract(obj)
         raise TypeError
@@ -74,8 +111,8 @@ class Vector(Point):
 
     def __round__(self, n=None):
         if n is not None:
-            return Vector(round(self.x, n), round(self.y, n), round(self.z, n))
-        return Vector(round(self.x), round(self.y), round(self.z))
+            return self.__class__(round(self.x, n), round(self.y, n), round(self.z, n))
+        return self.__class__(round(self.x), round(self.y), round(self.z))
 
     def add(self, obj):
         return self + obj
@@ -107,7 +144,7 @@ class Vector(Point):
 
     def cross(self, vector):
         """Return a Vector instance as the cross product of two vectors"""
-        return Vector((self.y * vector.z - self.z * vector.y),
+        return self.__class__((self.y * vector.z - self.z * vector.y),
                       (self.z * vector.x - self.x * vector.z),
                       (self.x * vector.y - self.y * vector.x))
 
@@ -164,7 +201,7 @@ class Vector(Point):
             y = self.y * math.cos(angle) - self.z * math.sin(angle)
             z = self.y * math.sin(angle) + self.z * math.cos(angle)
 
-        return Vector(x, y, z)
+        return self.__class__(x, y, z)
 
     @classmethod
     def from_points(cls, point1, point2):
